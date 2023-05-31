a3b36cac81501dce497e63c4667c5e3985028596
Alvaro <alvaromagu2002@gmail.com>
2023-05-31 20:13:47 +0200

    Add news component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { Header } from './components/header/header'
+import { News } from './components/news/news'
 
 function App () {
   return (
     <>
       <Header />
+      <main>
+        <News />
+      </main>
     </>
   )
 }
---
+++ src/components/news/news.tsx
@@ -0,0 +1,9 @@
+import { section } from './news.css'
+
+export function News () {
+  return (
+    <section className={section}>
+      News
+    </section>
+  )
+}
