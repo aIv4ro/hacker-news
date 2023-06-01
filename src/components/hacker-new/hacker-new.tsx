d272d88aee8d9e7226b65cc3adc43781c03963cd
Alvaro <alvaromagu2002@gmail.com>
2023-06-01 20:43:50 +0200

    Add intl time diff

--- src/components/hacker-new/hacker-new.tsx
+++ src/components/hacker-new/hacker-new.tsx
@@ -4,14 +4,40 @@ interface Props {
   hackerNew: IHackerNew
 }
 
-export function HackerNew ({ hackerNew }: Props) {
-  const date = new Date(hackerNew.time * 1000)
+const rtf = new Intl.RelativeTimeFormat(undefined, {
+  style: 'long', numeric: 'always'
+})
 
+type DateUnits = 'day' | 'hour' | 'minute' | 'second'
+
+const SECOND_DATE_UNITS_ENTRIES: Array<[DateUnits, number]> = [
+  ['day', 86400],
+  ['hour', 3600],
+  ['minute', 60],
+  ['second', 1]
+]
+
+const getTimeUnit = (diffInSeconds: number) => {
+  const [unit, seconds] = SECOND_DATE_UNITS_ENTRIES.find(
+    ([, value]) => diffInSeconds >= value
+  ) ?? SECOND_DATE_UNITS_ENTRIES[SECOND_DATE_UNITS_ENTRIES.length - 1]
+  return { unit, value: Math.floor(diffInSeconds / seconds) }
+}
+
+const getTimeDiff = (time: number) => {
+  const now = Date.now()
+  const diffInSeconds = (now - time) / 1000
+  const { value, unit } = getTimeUnit(diffInSeconds)
+  return rtf.format(value * -1, unit)
+}
+
+export function HackerNew ({ hackerNew }: Props) {
+  const timeDiff = getTimeDiff(hackerNew.time * 1000)
   return (
     <article>
       <header>{hackerNew.title}</header>
       <footer>
-        {hackerNew.score} {hackerNew.score === 1 ? 'point' : 'points'} by {hackerNew.by}
+        {hackerNew.score} {hackerNew.score === 1 ? 'point' : 'points'} by {hackerNew.by} {timeDiff}
       </footer>
     </article>
   )
